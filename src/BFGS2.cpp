5e08211cc9c95d38bf98b42e53e9b9aeeedcd0ac
SamP-S <sam.ps1@outlook.com>
2021-07-19 20:21:39 +0100

    Moved c++ files to respective include and src dirs

---
+++ src/BFGS2.cpp
@@ -0,0 +1,239 @@
+#define ARMA_DONT_USE_STD_MUTEX
+#include <armadillo>
+#include <iostream>
+#include <cmath>
+#include "linesearch.h"
+#include "forward_kinematics.h"
+#include <cstdlib>
+#include <ctime>
+arma::vec BFGS (double x, double y, double z, arma::vec X){
+
+    int count = 0;
+    LineSearch arm(0.057,0.365,0.430,0);
+    arma::dmat Bii(5,5,arma::fill::eye);
+    arma::dmat Bi = Bii;
+    arma::vec zeros(5, arma::fill::zeros);
+    arma::vec Xp(5, arma::fill::zeros), P(5,arma::fill::zeros), S(5,arma::fill::zeros), Y(5,arma::fill::zeros);
+    double A;
+    arm.set_goal(x,y,z);
+    double mu = 1000;
+    ForwardKinematics fk = ForwardKinematics();
+    int local_min_count = 0;
+    while(true){
+        double costdiff = 100;
+        double cost = 100;
+        double count = 0;
+        while (cost > 0.000001){
+            count++;
+            if(count > 100) break;
+            arma::vec costVec = arm.cost_function_gradient(X,0,mu);
+            cost = arm.cost_function(X,0,mu);
+            if(true){
+                std::cout << "Cost: " << cost << std::endl;
+                std::cout << "MU: " << mu << std::endl;
+            }
+            if (false) {
+                std::cout << "Bi: " << std::endl;
+                print_mat(Bi);
+            }
+            if (false) {
+                std::cout << "cost grad = ";
+                std::cout << costVec[0] << " ";  
+                std::cout << costVec[1] << " ";   
+                std::cout << costVec[2] << " ";
+                std::cout << costVec[3] << " ";
+                std::cout << costVec[4] << std::endl;
+            }
+            P = -(Bi*costVec);
+            if (false) {
+                // Debug
+                std::cout << "P = ";
+                std::cout << P[0] << " ";    
+                std::cout << P[1] << " ";  
+                std::cout << P[2] << " ";
+                std::cout << P[3] << " ";
+                std::cout << P[4] << std::endl;
+            }
+            
+            if(std::isnan(P(0))){
+                if(local_min_count > 10){
+                    std::cout << "Stuck in over 10 local minima" << std::endl;
+                    return X;
+                }
+                else{
+                    local_min_count++;
+                    std::cout << "Error: Local minima after " << local_min_count << " attempts" << std::endl;
+                    float q[5];
+                    std::cout << "Restarting from ";
+                    for(int i = 0; i<5;i++){
+                        q[i] = arm.min_angle[i] + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(arm.max_angle[i]-arm.min_angle[i])));
+                        X(i) = q[i];
+                        std::cout << q[i] << " ";
+                    }
+                    std::cout << std::endl;
+                    mu = 0;
+                    Bi = Bii;
+                    Xp = zeros;
+                    P = zeros; 
+                    S = zeros;
+                    Y = zeros;
+                    break;
+                }
+            }
+            Xp = arm.GoldenSearch(X, P, mu);
+            costdiff = abs(arm.cost_function(X,0,mu) - arm.cost_function(Xp,0,mu));
+            S = Xp - X;
+            if (false) {
+                //std::cout << "---------------------" << std::endl;
+                arma::vec pos = fk.GetExtendedPositionVector(X);
+                std::cout << "";
+                std::cout << pos[0] << ",";  
+                std::cout << pos[1] << ","; 
+                std::cout << pos[2] << std::endl;
+            }
+            if (false) {
+                std::cout << "X = ";
+                std::cout << X[0] << " ";    
+                std::cout << X[1] << " ";  
+                std::cout << X[2] << " ";
+                std::cout << X[3] << " ";
+                std::cout << X[4] << std::endl;
+            }
+            if (false) {
+                std::cout << "Xp = ";
+                std::cout << Xp[0] << " ";    
+                std::cout << Xp[1] << " ";  
+                std::cout << Xp[2] << " ";
+                std::cout << Xp[3] << " ";
+                std::cout << Xp[4] << std::endl;
+            }
+
+            arma::vec cost1 = arm.cost_function_gradient(Xp, 0, mu);
+            arma::vec cost2 = arm.cost_function_gradient(X,0, mu);      
+            if (false) {
+                std::cout << "Cost1: ";
+                std::cout << cost1[0] << " ";    
+                std::cout << cost1[1] << " ";    
+                std::cout << cost1[2] << " ";
+                std::cout << cost1[3] << " ";
+                std::cout << cost1[4] << std::endl; 
+
+                std::cout << "Cost2: ";
+                std::cout << cost2[0] << " ";    
+                std::cout << cost2[1] << " ";    
+                std::cout << cost2[2] << " ";
+                std::cout << cost2[3] << " ";
+                std::cout << cost2[4] << std::endl; 
+            }
+            Y = cost1 - cost2;
+
+            // https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
+            // (((S.t()*Y.t() + Y.t()*Bi*Y)*(S*S.t()))/(arma::powmat((S.t()*Y.t()),2)))-(((Bi*Y*S.t())+(S*Y.t()*Bi))/(S.t()*Y));
+
+            arma::mat St = S.t();
+            double aa = arma::as_scalar(S.t() * Y);
+            //arma::mat Yt = Y.t();
+            arma::mat ab = Y.t() * Bi;
+            double ac = arma::as_scalar(ab * Y);
+            arma::mat ad = S * S.t();
+            double ae = aa + ac;
+            arma::mat a = ae * ad;
+
+            arma::mat ba = Bi * Y;
+            arma::mat bb = ba * S.t();
+            arma::mat bc = S * Y.t();
+            arma::mat bd = bc * Bi;
+            arma::mat b = bb + bd;
+
+            double c = arma::as_scalar(S.t() * Y);
+            if (false) {
+                std::cout << "S = "; print_vec(S);
+                std::cout << "Y = "; print_vec(Y);
+            }
+
+            double da = c * c;
+            arma::mat db = a * (1/da);
+            arma::mat dc = b * (1/c);
+            arma::mat d = db - dc;
+            Bi = Bi + d;
+            X=Xp;
+            count += 1;
+            if (false) {
+                std::cout << "db = "; print_mat(db);
+                std::cout << "dc = "; print_mat(dc);
+                std::cout << "d = "; print_mat(d);
+            }
+            if (false) {
+                std::cout << "Bi: " << std::endl;
+                print_mat(Bi);
+            }
+            if(count > 50){cost = 0;}
+        }
+        if(arm.dist_to_goal(X,0) > 0.002){
+        //if(mu>0.001){
+            mu = mu*0.9;
+        }
+        else{
+            return X;
+        }
+    }
+    //arma::vec good;
+    //good << 0.540419489324968 << 0.334426881574699 << 0.658670308915048 << 0.600956360426415 << 0 << arma::endr;
+    //std::cout << arm.cost_function(good,0,mu) << std::endl;
+    //std::cout << arm.cost_function_gradient(good,0,mu) << std::endl;
+    return X;
+}
+
+
+int main (){
+    LineSearch ls(0.057,0.365,0.430,0);
+    ForwardKinematics fktoo;
+    arma::vec yeet;
+    yeet << 0 << 0 << 0 << 0 << 0 << arma::endr;
+    arma::vec start;
+    start << 0.2 << 0.2 << 0.2 << 0.2 << 0.0 << arma::endr;
+    double x, y, z = 0;
+
+    srand (static_cast <unsigned> (time(0)));
+
+    x = 0.25;
+    y = 0.15;
+    z = 0.2;
+    arma::vec position;
+    position << x << y << z << arma::endr;
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Goal: " << x << ", " << y << ", " << z << std::endl;
+    std::cout << "Goal in-bounds test: " << ls.InBoundsPos(position) << std::endl;
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Starting motor angles: ";
+    print_vec(start);
+    std::cout << "Startng Coordinates: ";
+    print_vec(fktoo.GetExtendedPositionVector(start),3);
+    std::cout << "Start in-bounds test: " << ls.InBounds(start) << std::endl;
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Running BFGS Algorithm..." << std::endl;
+    arma::dmat end = BFGS(x, y, z, start);
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Caluclated Motor Angles: ";
+    std::cout << end[0] << " ";    
+    std::cout << end[1] << " ";    
+    std::cout << end[2] << " ";
+    std::cout << end[3] << " ";
+    std::cout << end[4] << std::endl;
+    arma::vec::fixed<5> vmat = end.as_col();
+    
+    ls.set_goal(x,y,z);
+    std::cout << "Final Cost = " << ls.cost_function(vmat, 0, 0.00001) << std::endl;
+    std::cout << "Final Grad = " << ls.cost_function_gradient(vmat, 0, 0.00001) << std::endl;
+    
+    arma::vec pos = fktoo.GetExtendedPositionVector(vmat);
+    std::cout << "---------------------" << std::endl;
+    std::cout << "FK Calculated Position Based off Angles: ";
+    std::cout << pos[0] << " ";    
+    std::cout << pos[1] << " ";    
+    std::cout << pos[2] << std::endl;
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Press Enter Key to Continue..." << std::endl;     
+    std::cin.get();
+    return 0;
+}
---
+++ src/linesearch.cpp
@@ -0,0 +1,221 @@
+#define ARMA_DONT_USE_STD_MUTEX
+#include <armadillo>
+#include <cmath>
+
+#include "linesearch.h"
+
+#define USE_BOUNDS true
+
+void print_mat(arma::mat mat, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << mat[i * m + j] << "   ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void print_vec(arma::vec v, int size) {
+    std::cout << v[0];
+    for (int i = 1; i < size; i++) {
+        std::cout << "   " << v[i] ;
+    }
+    std::cout << std::endl;
+}
+
+LineSearch::LineSearch(double r_inner, double r_outer, double h_max, double h_min) {
+    radius_inner = r_inner;
+    radius_outer = r_outer;
+    height_max = h_max;
+    height_min = h_min;
+
+    fk = ForwardKinematics();
+}
+
+void LineSearch::set_goal(double x, double y, double z) {
+    goal_x = x;
+    goal_y = y;
+    goal_z = z;
+}
+
+double LineSearch::cost_function(arma::vec5 q, double d, double MU) {
+    arma::vec actual_coords = fk.GetExtendedPositionVector(q);
+    double actual_x = actual_coords(0);
+    double actual_y = actual_coords(1);
+    double actual_z = actual_coords(2);
+
+    double radius = sqrt(pow(actual_x,2) + pow(actual_y,2) + pow(actual_z - 0.065,2));
+
+    double cost = pow(goal_x - actual_x, 2) + pow(goal_y - actual_y, 2) + pow(goal_z - actual_z, 2);
+    
+    if (USE_BOUNDS) {
+        for(int i = 0; i < CRAP_NUM_REVOLUTE_FRAMES; i++) {
+            cost -= MU * (log(max_angle[i] - q[i]) + log(q[i] - min_angle[i]));
+        }
+        //cost += MU * (log(radius_outer - radius));
+    }
+    
+    return cost;
+}
+
+double LineSearch::dist_to_goal(arma::vec5 q, double d) {
+    arma::vec actual_coords = fk.GetExtendedPositionVector(q);
+    double actual_x = actual_coords(0);
+    double actual_y = actual_coords(1);
+    double actual_z = actual_coords(2);
+
+    double cost = sqrt(pow(goal_x - actual_x, 2) + pow(goal_y - actual_y, 2) + pow(goal_z - actual_z, 2));
+
+    return cost;
+}
+
+arma::vec LineSearch::cost_function_gradient(arma::vec5 q, double d, double MU) {
+    arma::vec grad(5, arma::fill::zeros);
+    double q0 = q(0);
+    double q1 = q(1) + arma::datum::pi / 2;;
+    double q2 = q(2);
+    double q3 = q(3) - arma::datum::pi / 2;;
+    double q4 = q(4);
+    arma::vec actual_coords = fk.GetExtendedPositionVector(q);
+    double actual_x = actual_coords(0);
+    double actual_y = actual_coords(1);
+    double actual_z = actual_coords(2);
+    //distance to goal
+    grad(0) = -0.095*goal_y*cos(q0 + q1) + 0.13*goal_y*sin(q0 + q3 + q1 + q2) - 0.095*goal_y*cos(q0 + q1 + q2) - 0.095*goal_y*cos(q0 - q1) - 0.13*goal_y*sin(q0 - q3 - q1 - q2) - 0.095*goal_y*cos(q0 - q1 - q2) + 0.095*goal_x*sin(q0 - q1 - q2) + 0.095*goal_x*sin(q0 - q1) - 0.13*goal_x*cos(q0 - q3 - q1 - q2) + 0.095*goal_x*sin(q0 + q1) + 0.13*goal_x*cos(q0 + q3 + q1 + q2) + 0.095*goal_x*sin(q0 + q1 + q2);
+    grad(1) = -0.095*goal_y*cos(q0 + q1) + 0.13*goal_y*sin(q0 + q3 + q1 + q2) + 0.095*goal_y*cos(q0 - q1) + 0.13*goal_y*sin(q0 - q3 - q1 - q2) + 0.095*goal_y*cos(q0 - q1 - q2) + 0.012350*cos(q1) + 0.01235*cos(q1 + q2) - 0.190*goal_z*cos(q1) - 0.095*goal_y*cos(q0 + q1 + q2) - 0.01690*sin(q3 + q1 + q2) + 0.26*goal_z*sin(q3 + q1 + q2) - 0.19*goal_z*cos(q1 + q2) + 0.095*goal_x*sin(q0 + q1) - 0.095*goal_x*sin(q0 - q1) + 0.095*goal_x*sin(q0 + q1 + q2) - 0.095*goal_x*sin(q0 - q1 - q2) + 0.13*goal_x*cos(q0 - q3 - q1 - q2) + 0.13*goal_x*cos(q0 + q3 + q1 + q2);
+    grad(2) = 0.13*goal_y*sin(q0 + q3 + q1 + q2) + 0.13*goal_y*sin(q0 - q3 - q1 - q2) + 0.095*goal_y*cos(q0 - q1 - q2) - 0.01805*sin(q2) + 0.01235*cos(q1 + q2) - 0.095*goal_y*cos(q0 + q1 + q2) - 0.01690*sin(q3 + q1 + q2) + 0.26*goal_z*sin(q3 + q1 + q2) - 0.19*goal_z*cos(q1 + q2) - 0.02470*cos(q2 + q3) + 0.095*goal_x*sin(q0 + q1 + q2) - 0.095*goal_x*sin(q0 - q1 - q2) + 0.13*goal_x*cos(q0 - q3 - q1 - q2) + 0.13*goal_x*cos(q0 + q3 + q1 + q2);
+    grad(3) = 0.13*goal_y*sin(q0 + q3 + q1 + q2) + 0.13*goal_y*sin(q0 - q3 - q1 - q2) - 0.02470*cos(q3) - 0.01690*sin(q3 + q1 + q2) + 0.26*goal_z*sin(q3 + q1 + q2) - 0.02470*cos(q2 + q3) + 0.13*goal_x*cos(q0 - q3 - q1 - q2) + 0.13*goal_x*cos(q0 + q3 + q1 + q2);
+    grad(4) = 0;
+    //motor angle limits
+    if(USE_BOUNDS){
+        for(int i = 0; i < CRAP_NUM_REVOLUTE_FRAMES; i++) {
+            double a = -1/(max_angle[i] - q(i));
+            double b = 1/(q(i) - min_angle[i]);
+            grad(i) = grad(i) - MU*(a + b);
+        }
+        //grad(2) = grad(2) + MU*((-0.009025000000*sin(q2) - 0.01235000000*cos(q3 + q2))/(sqrt(-0.02470000000*sin(q3) + 0.01805000000*cos(q2) + 0.03495000000 - 0.02470000000*sin(q3 + q2))*(-1.*radius_outer + sqrt(0.03495 - 0.0247*sin(q3) + 0.01805*cos(q2) - 0.02470000000*sin(q3 + q2)))));
+        //grad(3) = grad(3) + MU*(-0.01235000000*(cos(q3 + q2) + cos(q3))/((-radius_outer + sqrt(0.03495 - 0.0247*sin(q3) + 0.01805*cos(q2) - 0.02470000000*sin(q3 + q2)))*sqrt(-0.02470000000*sin(q3) + 0.01805000000*cos(q2) + 0.03495000000 - 0.02470000000*sin(q3 + q2))));
+    }
+    return grad;
+}
+
+int LineSearch::InBoundsPos(arma::vec pos) {
+    double actual_x = pos(0);
+    double actual_y = pos(1);
+    double actual_z = pos(2);
+    double radius = sqrt(pow(actual_x,2) + pow(actual_y,2) + pow(actual_z - 0.065,2));
+    double radius2 = sqrt(pow(actual_x,2) + pow(actual_y,2));
+    if (radius > radius_outer) {
+        return 1;
+    }
+    else if (actual_z < height_min) {
+        return 2;
+    }
+    else if (radius2 < radius_inner && actual_z < 0.2){
+        return 3;
+    }
+    return 0;
+}
+
+int LineSearch::InBounds(arma::vec angles) {
+    arma::vec actual_coords = fk.GetExtendedPositionVector(angles);
+    double actual_x = actual_coords(0);
+    double actual_y = actual_coords(1);
+    double actual_z = actual_coords(2);
+    double radius = sqrt(pow(actual_x,2) + pow(actual_y,2) + pow(actual_z - 0.065,2));
+    if(USE_BOUNDS){
+        for (int i = 0; i < 5; i++) {
+            if (angles[i] > max_angle[i] || angles[i] < min_angle[i]) {
+                return i+3;
+            }
+        }
+    }
+    if (radius > radius_outer) {
+        return 1;
+    }
+    if (actual_z < height_min) {
+        return 2;
+    }
+    return 0;
+}
+
+
+// https://en.wikipedia.org/wiki/Golden-section_search
+
+// tolerance == numerical tolerance threshold
+// tau == golden ration number for sectioni
+
+// x_min = where the moters are rn
+// x_max = where the moters are if they go out of bounds
+// w_min = lower tau search point
+// w_max = upper tau search point
+
+// Golden Search function
+// @param current_pos - current angles of motors as vector
+// @param direction - direction of search
+// @return step as float value
+arma::vec LineSearch::GoldenSearch(arma::vec current_pos, arma::vec direction, double mu) {
+    // CONSTANTS
+    arma::vec dir_norm = arma::normalise(direction);
+    float tolerance = 0.00000001;
+    float tau = 2 / (1 + sqrt(5));
+    float dir_step = 0.0001;
+    arma::vec x_min = current_pos;
+    arma::vec x_max = arma::vec::fixed<5>();
+    arma::vec start_min = x_min;
+    arma::vec start_max = x_max;
+    x_max = current_pos;
+    int dir_count = 0;
+    arma::vec dir_inc = dir_norm * dir_step;
+
+    //std::cout << "dir_norm = "; print_vec(dir_norm);
+
+    while (InBounds(x_max) == 0) {
+        x_max = x_max + dir_inc;
+        dir_count += 1;
+    }
+    //std::cout << InBounds(x_max) << std::endl;
+    if (dir_count == 1) {
+        std::cout << "wah" << std::endl;
+    }
+    while (InBounds(x_max) != 0) {
+         x_max = x_max - dir_inc * 0.5;
+    }
+    //std::cout << InBounds(x_max) << std::endl;
+    //std::cout << dir_count << std::endl;
+    //   for (int i = 0; i < 5; i++) {
+    //     x_max[i] = (direction[i] >= 0) ?  this->max_angle[i] : this->min_angle[i];
+    //   }
+
+    // VARIALBES
+    int k = 0; // interations;
+    arma::vec w_min = tau * x_min + (1 - tau) * x_max; // initialise lower tau search point
+    arma::vec w_max = (1 - tau) * x_min + tau * x_max; // initialise upper tau search point
+
+    while (arma::max(arma::abs(x_max - x_min)) > tolerance) {
+        float j_w_min = this->cost_function(w_min,0,mu);
+        float j_w_max = this->cost_function(w_max,0,mu);
+        if (j_w_min < j_w_max) {
+            x_max = w_max;
+            w_max = w_min;
+            w_min = tau * x_min + (1 - tau) * x_max;
+        } else {
+            x_min = w_min;
+            w_min = w_max;
+            w_max = (1 - tau) * x_min + tau * x_max;
+        }
+        k += 1;
+        //std::cout << k << ": " << arma::max(arma::abs(x_max - x_min)) << std::endl;
+    }
+    arma::vec v = (x_min + x_max) * 0.5;
+    if (false) {
+        std::cout << "start_min: " << this->cost_function(start_min, 0, 0.1) << std::endl; 
+        std::cout << "final_res: " << this->cost_function(v, 0, 0.1) << std::endl;
+        std::cout << "start_max: " << this->cost_function(start_max, 0, 0.1) << std::endl; 
+    }
+    if (false) {
+        std::cout << "x_min: " << this->cost_function(x_min, 0, 0.1) << std::endl; 
+        std::cout << "x_max: " << this->cost_function(x_max, 0, 0.1) << std::endl; 
+    }
+    return v;
+}
